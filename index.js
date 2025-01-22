5ab42505332ffd9ec1ad60e6bdc3027d9c058dcc
eisemsi2 <prajjwalk21@iitk.ac.in>
2025-01-23 03:37:56 +0530

    frontend address added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://wb-five-blue.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
